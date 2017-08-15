c431e3049dec8a2cf02738942321b5ea711ca09f
Thejus Jain <thejusjain101@gmail.com>
2017-08-15 22:49:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,27 @@
 console.log('Loaded!');
 
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function (){
   
   //create the request
-  //var request = new XMLHttpRequest();
+  var request = new XMLHttpRequest();
   
   //capture the response and store it in a variable
+  request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE){
+        //take some action
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();    
+        }
+    }
+  };
   
+  //make the request
+  request.open('GET','http://http://thejusjain101.imad.hasura-app.io/counter',true);
+  request.send(null);
+ 
   
-  //Render the variable in the span element
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString(); 
     
 };
