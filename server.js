9f264752aba0feda7c008cfe7841902ee745cef6
Thejus Jain <thejusjain101@gmail.com>
2017-08-15 16:12:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function(req,res) {
     var articleName = req.param.articleName;
-  res.send(createTemplate(article[articleName]));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function(req,res) {
