5fdfd5dc072194adaf11f46775ac9dd6a0004711
Thejus Jain <thejusjain101@gmail.com>
2017-08-15 17:00:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,11 @@ element.innerHTML = "New Value";
 
 //move image
 var img = document.getElementById('madi');
-    img.onclick = function () {
-        img.style.marginLeft = '100px';
-    }
+var marginLeft = 0;
+function moveRight () {
+     marginLeft = marginLeft + 10;
+     img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function () {
+    var interval = setInterval(moveRight, 100);
+}
