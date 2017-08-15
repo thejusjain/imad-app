495928a245380a54fb02e46edff1f1b3350a518f
Thejus Jain <thejusjain101@gmail.com>
2017-08-15 16:56:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,12 @@
 console.log('Loaded!');
 
+//cahnge HTML element
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
+
+
+//move image
+var img = document.getElementById('madi');
+    img.onclick = function () {
+        img.style.marginLeft = '100px';
+    }
