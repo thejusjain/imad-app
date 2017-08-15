8ce27634f9c5f80e26ccdfd57461a81326612c72
Thejus Jain <thejusjain101@gmail.com>
2017-08-15 23:10:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,3 +25,20 @@ button.onclick = function (){
   
     
 };
+
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value; 
+var submit = document.getElementById('sub_btn');
+submit.onclick = function () {
+
+
+    var names = ['name1', 'name2', 'name3','name4'];
+    var list = '';
+    for (var i = 0; i < names.length; i++ ){
+        list = '<li>' + names[i] + '</li>';
+        
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+}
