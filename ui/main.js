2064c5e5273459225450f83add6acc6ac43c3045
Thejus Jain <thejusjain101@gmail.com>
2017-08-15 22:36:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,20 @@ function moveRight () {
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
 }
+
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function (){
+  
+  //create the request
+  //var request = new XMLHttpRequest();
+  
+  //capture the response and store it in a variable
+  
+  
+  //Render the variable in the span element
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString(); 
+    
+};
