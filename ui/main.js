ce32ad78f34045b9d66cdaa97b591efd719cca02
Thejus Jain <thejusjain101@gmail.com>
2017-08-15 22:38:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,6 @@
 console.log('Loaded!');
 
-//cahnge HTML element
-var element = document.getElementById("main-text");
-element.innerHTML = "New Value";
 
-
-//move image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-     marginLeft = marginLeft + 2;
-     img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-}
-
-var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function (){
   
