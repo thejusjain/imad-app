53333a092efa57d0cdcfd3df18c625aa71759636
Thejus Jain <thejusjain101@gmail.com>
2017-09-14 21:09:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,9 +13,28 @@ button.onclick = function(){
                 span.innerHTML = counter.toString();    
             }
         }
-    };
+    }
   //make the request
     request.open('GET', 'http://thejusjain101.imad.hasura-app.io/counter', true);
     request.send(null);
     
-};
+}
+
+//submit name and display
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('sub_btn');
+submit.onclick = function(){
+    //make request to the server 
+    
+    //get the names and render it into the html
+    var listItems = ['name1','name2','name3','name4'];
+    list = '';
+    
+    for(var i = 0; i < listItems.length; i++) {
+        list += '<li>' + listItems[i] + '</li>';
+    }
+    
+    var ul = document.getElementById()
+}
