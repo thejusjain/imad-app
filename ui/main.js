b0c8c82eb44fdc338423b8307c7792e1a86bfbcd
Thejus Jain <thejusjain101@gmail.com>
2017-09-14 19:30:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,16 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
     //make the request to the server
+    var request = new XMLHttpRequest();
     
     //capture vthe response to a variable
-    
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status == 200){
+                
+            }
+        }
+    }
     //render the vaiable to input the value to the correct span
     counter = counter + 1;
     var span = document.getElementById('count');
