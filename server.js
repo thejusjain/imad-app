5c433017cb193c9e6141b18bd65f5dcfb94ade78
Thejus Jain <thejusjain101@gmail.com>
2017-09-14 08:09:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    'article-one': {
+    article: {
       title: 'Thejus Jain | article one',
       heading: 'Hi this is article one',
       date: 'September 13, 2017',
