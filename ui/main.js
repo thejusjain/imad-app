ef43ec3c6ed68372b14dbd791b149cb54c77a551
Thejus Jain <thejusjain101@gmail.com>
2017-09-14 22:44:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick = function(){
                 names = JSON.parse(names);
                 
                 for(var i = 0; i < names.length; i++) {
-                    list = '<li>' + names[i] + '</li>';
+                    list += '<li>' + names[i] + '</li>';
                 }
                 
                 var ul = document.getElementById('list');
