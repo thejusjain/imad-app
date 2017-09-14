3b456904d6b3aad74644fa94b1aeb363fc580370
Thejus Jain <thejusjain101@gmail.com>
2017-09-14 12:48:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,11 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-
+var counter = 0;
+app.get('/counter', function (req, res){
+   counter = counter + 1; 
+   res.send(counter.toString());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
