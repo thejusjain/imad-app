04e5c115be890605e43abe78e84a54bcd5a8a9c1
Thejus Jain <thejusjain101@gmail.com>
2017-09-19 19:44:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,9 +106,23 @@ app.get('/submit-name', function (req, res){
 });
 
 
-app.get('/:articleName', function (req, res) {
+app.get('/article/:articleName', function (req, res) {
   var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+  
+  pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err, result){
+     if(err){
+         res.status(500).send(err.toString());
+     } 
+     else{
+         if(result.rows.length === 0){
+             res.status(404).send('Page not found');
+         }
+         else{
+             var articleData = result.rows[0];
+             res.send(createTemplate(articleData));
+         }
+     }
+  });
 });
 
 app.get('/', function (req, res) {
