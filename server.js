33da33ffaccce7f0a49a90c7458a877a1f2c3540
Thejus Jain <thejusjain101@gmail.com>
2017-09-19 19:22:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/test-db', function(req, res){
            res.status(500).send(err.toString());
        }
        else{
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
        }
    })
 });
